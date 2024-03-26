9b19e1a7d30364c56e1cf6a59bcbdb63d4552f67
SilvanaCuartas <165075107+SilvanaCuartasS@users.noreply.github.com>
2024-03-26 15:51:51 -0500

    Add files via upload

---
+++ Taller11/script.js
@@ -0,0 +1,13 @@
+const nombreUsuario = prompt("Escribe tu nombre");
+
+const apellidoUsuario = prompt("Escribe tu apellido");
+
+const añoDelUsuario = parseInt(prompt("Ingrese su año de nacimiento"));
+
+const calculo = 2024-añoDelUsuario;
+
+const diaDelUsuario = parseInt(prompt("Ingrese su día de nacimiento"));
+
+const mesDelUsuario = prompt("Escribe tu mes de nacimiento");
+
+console.log("Mi nombre es", nombreUsuario,apellidoUsuario,"mi edad es", calculo, "y nací el",diaDelUsuario, "de",mesDelUsuario, "del", añoDelUsuario);
